14c60c53499d5e347f986f6d85215d7349c8bebf
josno <jnsenoren@gmail.com>
2020-09-16 19:26:06 +0400

    set up app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import styled from "styled-components";
 
 import Wine from "./Pages/Wine";
 import Main from "./Pages/Main";
 import Pair from "./Pages/Pair";
 import Recommend from "./Pages/Recommend";
 
+import "./App.css";
+
 const App = () => {
 	return (
-		<div>
+		<>
+			<HeaderStyle>
+				<H1Style>
+					You'll be fine!
+					<br />
+					<Bold>Drink some wine.</Bold>
+				</H1Style>
+			</HeaderStyle>
 			<Switch>
 				<Route exact path='/'>
 					<Main />
@@ -23,8 +33,33 @@ const App = () => {
 					<Recommend />
 				</Route>
 			</Switch>
-		</div>
+			<Footer>Made By Joanne</Footer>
+		</>
 	);
 };
+
+const HeaderStyle = styled.header`
+	height: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+`;
+
+const H1Style = styled.h1`
+	font-size: 1.4em;
+	text-align: right;
+	padding: 10px;
+	margin: 0px;
+`;
+
+const Bold = styled.span`
+	font-weight: bold;
+	color: red;
+`;
+
+const Footer = styled.footer`
+	position: absolute;
+	bottom: auto;
+`;
 
 export default App;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "react-router-dom";
+import styled from "styled-components";
 
 const Main = (props) => {
-	return <div>This is main</div>;
+	return (
+		<MainStyles>
+			<button>I want to drink.</button>
+			<button>I want to drink and eat.</button>
+			<button>I want to recommend a wine.</button>
+		</MainStyles>
+	);
 };
 
+const MainStyles = styled.main`
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+`;
 export default Main;
